2995febec82134c29525ecdd45d20d77b6a6bbfb
Harsh Srivastava <63211890+wolverinezgit@users.noreply.github.com>
2022-05-13 21:12:39 +0530

    Delete js directory

--- js/main.js
+++
@@ -1,6 +0,0 @@
-let btnClear = document.querySelector('button.reset');
-let inputs = document.querySelectorAll('input');
- 
-btnClear.addEventListener('click', () => {
-    inputs.forEach(input =>  input.value = '');
-});
